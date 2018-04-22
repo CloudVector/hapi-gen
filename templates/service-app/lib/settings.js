9e43ca3716cf477acc998e6c31ff8f9ce87a6e54
Tamara Dukai <tamara@dukai.net>
2018-04-22 23:19:11 +0200

    Finished first template generator

--- templates/service-app/lib/settings.js
+++ templates/service-app/lib/settings.js
@@ -3,29 +3,24 @@
 const files = require('./files.js');
 const path = require('path');
 const PLUGINS_FOLDER = 'plugins';
-//const config = require('config');
-//const MongoClient = require('mongodb').MongoClient;
 
-const settings = async () => {
+const settings = async (dbmodule) => {
     let list = files.directories(path.join(__dirname, '..', PLUGINS_FOLDER));
     let result = {
         debug: (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'dev'),
         path: path.join(__dirname, '..'),
-        client: null,
-        database: null,
+        db: null,
+        close: null,
         facade: {}
     };
 
-    // Add mongoDb connection (this app using a single DB), remove comment if required
-    /*
-    try {
-        result.database = await MongoClient.connect(config.db.url);
-        result.db = result.database.db(config.db.dbname);
-    } catch (e) {
-        console.error('Failed to connect to mongoDb: ', e);
-        process.exit(1);
+    // Initialize database if module is specified
+    if (dbmodule !== null) {
+        let initDb = require(dbmodule);
+        let tmp = await initDb();
+        result.db = tmp.db;
+        result.close = tmp.close;
     }
-    */
 
     // Run for all widget look for repositories
     //console.log('Registering repositories...');
